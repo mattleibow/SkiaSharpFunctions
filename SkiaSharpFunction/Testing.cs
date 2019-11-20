639e1acb14ec9b5e046710e92567101b96777723
Matthew Leibowitz <mattleibow@live.com>
2019-11-20 07:16:36 +0200

    Open this puppy up

--- SkiaSharpFunction/Testing.cs
+++ SkiaSharpFunction/Testing.cs
@@ -12,7 +12,7 @@ namespace SkiaSharpFunction
 	public static class Testing
     {
         [FunctionName("testing")]
-        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
